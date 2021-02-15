9228724152a738de64bdb617151a241f977cb1cf
Nitraniel <52861371+Nitraniel@users.noreply.github.com>
2021-02-15 15:04:00 +0300

    lesson5

---
+++ js.js
@@ -0,0 +1,15 @@
+
+function openMenu() {
+    var x = document.getElementById('hide');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function closeMenu() {
+    var x = document.getElementById('hide');
+    x.style.display = "none";
+}
+
